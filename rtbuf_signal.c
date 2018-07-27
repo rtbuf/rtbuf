c46c53132886b4e71213edc30fa7d1c6d7ae4670
Thomas de Grivel <thoxdg@gmail.com>
2018-07-27 21:54:10 +0200

    keep some space in buffers

--- rtbuf_signal.c
+++ rtbuf_signal.c
@@ -9,11 +9,11 @@ const char     *rtbuf_lib_name = "signal";
 unsigned long   rtbuf_lib_ver = RTBUF_LIB_VER;
 s_rtbuf_lib_fun rtbuf_lib_fun[] = {
   { "sinus", rtbuf_signal_sinus, rtbuf_signal_sinus_start, 0,
-    sizeof(s_rtbuf_signal_sinus_data) / sizeof(double),
+    sizeof(s_rtbuf_signal_sinus_data) / sizeof(double) + 1,
     sizeof(double),
     (const char*[]) { "frequency", "amplitude", 0 } },
   { "square", rtbuf_signal_square, rtbuf_signal_square_start, 0,
-    sizeof(s_rtbuf_signal_square_data) / sizeof(double),
+    sizeof(s_rtbuf_signal_square_data) / sizeof(double) + 1,
     sizeof(double),
     (const char*[]) { "frequency", "amplitude", "pulse", 0 } },
   { 0, 0, 0, 0, 0, 0, 0 }
