39ef305e5deac32b471fbc3ca6c81bbeef71e192
Thomas de Grivel <thoxdg@gmail.com>
2018-08-03 21:00:05 +0200

    build using autotools

---
+++ dependencies.mk
@@ -0,0 +1,129 @@
+symbol.o: symbol.c /usr/include/stdio.h /usr/include/sys/cdefs.h \
+  /usr/include/machine/cdefs.h /usr/include/sys/_null.h \
+  /usr/include/sys/_types.h /usr/include/machine/_types.h \
+  /usr/include/sys/types.h /usr/include/sys/endian.h \
+  /usr/include/sys/_endian.h /usr/include/machine/endian.h \
+  /usr/include/string.h /usr/include/strings.h symbol.h
+rtbuf_type.o: rtbuf_type.c /usr/include/stdio.h /usr/include/sys/cdefs.h \
+  /usr/include/machine/cdefs.h /usr/include/sys/_null.h \
+  /usr/include/sys/_types.h /usr/include/machine/_types.h \
+  /usr/include/sys/types.h /usr/include/sys/endian.h \
+  /usr/include/sys/_endian.h /usr/include/machine/endian.h \
+  /usr/include/stdlib.h /usr/include/string.h /usr/include/strings.h \
+  rtbuf_type.h rtbuf_defs.h symbol.h
+rtbuf_fun.o: rtbuf_fun.c /usr/include/assert.h /usr/include/sys/cdefs.h \
+  /usr/include/machine/cdefs.h /usr/include/stdio.h \
+  /usr/include/sys/_null.h /usr/include/sys/_types.h \
+  /usr/include/machine/_types.h /usr/include/sys/types.h \
+  /usr/include/sys/endian.h /usr/include/sys/_endian.h \
+  /usr/include/machine/endian.h /usr/include/stdlib.h \
+  /usr/include/strings.h rtbuf.h rtbuf_defs.h rtbuf_type.h rtbuf_fun.h \
+  symbol.h
+rtbuf.o: rtbuf.c /usr/include/assert.h /usr/include/sys/cdefs.h \
+  /usr/include/machine/cdefs.h /usr/include/stdio.h \
+  /usr/include/sys/_null.h /usr/include/sys/_types.h \
+  /usr/include/machine/_types.h /usr/include/sys/types.h \
+  /usr/include/sys/endian.h /usr/include/sys/_endian.h \
+  /usr/include/machine/endian.h /usr/include/stdlib.h \
+  /usr/include/string.h /usr/include/strings.h rtbuf.h rtbuf_defs.h \
+  rtbuf_type.h rtbuf_fun.h symbol.h
+rtbuf_lib.o: rtbuf_lib.c /usr/include/assert.h /usr/include/sys/cdefs.h \
+  /usr/include/machine/cdefs.h /usr/include/dlfcn.h /usr/include/stdio.h \
+  /usr/include/sys/_null.h /usr/include/sys/_types.h \
+  /usr/include/machine/_types.h /usr/include/sys/types.h \
+  /usr/include/sys/endian.h /usr/include/sys/_endian.h \
+  /usr/include/machine/endian.h /usr/include/stdlib.h \
+  /usr/include/string.h /usr/include/strings.h rtbuf.h rtbuf_defs.h \
+  rtbuf_type.h rtbuf_fun.h symbol.h rtbuf_lib.h
+rtbuf_cli.o: rtbuf_cli.c /usr/include/assert.h /usr/include/sys/cdefs.h \
+  /usr/include/machine/cdefs.h /usr/local/include/cli.h \
+  /usr/include/pthread.h /usr/include/sys/types.h \
+  /usr/include/sys/endian.h /usr/include/sys/_endian.h \
+  /usr/include/sys/_types.h /usr/include/machine/_types.h \
+  /usr/include/machine/endian.h /usr/include/sys/time.h \
+  /usr/include/sys/select.h /usr/include/time.h /usr/include/sys/_null.h \
+  /usr/include/sys/_time.h /usr/include/sys/signal.h \
+  /usr/include/machine/signal.h /usr/include/sys/siginfo.h \
+  /usr/include/limits.h /usr/include/sys/limits.h \
+  /usr/include/machine/limits.h /usr/include/sys/syslimits.h \
+  /usr/include/sched.h /usr/include/stdlib.h /usr/include/stdio.h \
+  /usr/include/unistd.h /usr/include/sys/unistd.h rtbuf.h rtbuf_defs.h \
+  rtbuf_type.h rtbuf_fun.h symbol.h rtbuf_lib.h
+rtbuf_music.o: rtbuf_music.c rtbuf.h rtbuf_defs.h rtbuf_type.h \
+  rtbuf_fun.h symbol.h rtbuf_music.h
+rtbuf_signal.o: rtbuf_signal.c /usr/include/math.h \
+  /usr/include/sys/_types.h /usr/include/machine/_types.h \
+  /usr/include/sys/limits.h /usr/include/sys/cdefs.h \
+  /usr/include/machine/cdefs.h /usr/include/machine/limits.h \
+  /usr/include/stdio.h /usr/include/sys/_null.h /usr/include/sys/types.h \
+  /usr/include/sys/endian.h /usr/include/sys/_endian.h \
+  /usr/include/machine/endian.h rtbuf.h rtbuf_defs.h rtbuf_type.h \
+  rtbuf_fun.h symbol.h rtbuf_lib.h rtbuf_music.h rtbuf_signal.h \
+  rtbuf_signal_type.h
+rtbuf_signal_sinus.o: rtbuf_signal_sinus.c /usr/include/math.h \
+  /usr/include/sys/_types.h /usr/include/machine/_types.h \
+  /usr/include/sys/limits.h /usr/include/sys/cdefs.h \
+  /usr/include/machine/cdefs.h /usr/include/machine/limits.h \
+  /usr/include/stdio.h /usr/include/sys/_null.h /usr/include/sys/types.h \
+  /usr/include/sys/endian.h /usr/include/sys/_endian.h \
+  /usr/include/machine/endian.h rtbuf.h rtbuf_defs.h rtbuf_type.h \
+  rtbuf_fun.h symbol.h rtbuf_signal.h rtbuf_music.h
+rtbuf_signal_square.o: rtbuf_signal_square.c /usr/include/math.h \
+  /usr/include/sys/_types.h /usr/include/machine/_types.h \
+  /usr/include/sys/limits.h /usr/include/sys/cdefs.h \
+  /usr/include/machine/cdefs.h /usr/include/machine/limits.h \
+  /usr/include/stdio.h /usr/include/sys/_null.h /usr/include/sys/types.h \
+  /usr/include/sys/endian.h /usr/include/sys/_endian.h \
+  /usr/include/machine/endian.h rtbuf.h rtbuf_defs.h rtbuf_type.h \
+  rtbuf_fun.h symbol.h rtbuf_signal.h rtbuf_music.h
+rtbuf_signal_adsr.o: rtbuf_signal_adsr.c rtbuf.h rtbuf_defs.h \
+  rtbuf_type.h rtbuf_fun.h symbol.h rtbuf_signal.h rtbuf_music.h
+rtbuf_signal_synth.o: rtbuf_signal_synth.c /usr/include/stdio.h \
+  /usr/include/sys/cdefs.h /usr/include/machine/cdefs.h \
+  /usr/include/sys/_null.h /usr/include/sys/_types.h \
+  /usr/include/machine/_types.h /usr/include/sys/types.h \
+  /usr/include/sys/endian.h /usr/include/sys/_endian.h \
+  /usr/include/machine/endian.h /usr/include/strings.h rtbuf.h \
+  rtbuf_defs.h rtbuf_type.h rtbuf_fun.h symbol.h rtbuf_signal.h \
+  rtbuf_music.h
+rtbuf_sndio.o: rtbuf_sndio.c /usr/include/sndio.h \
+  /usr/include/sys/types.h /usr/include/sys/cdefs.h \
+  /usr/include/machine/cdefs.h /usr/include/sys/endian.h \
+  /usr/include/sys/_endian.h /usr/include/sys/_types.h \
+  /usr/include/machine/_types.h /usr/include/machine/endian.h \
+  /usr/include/stdio.h /usr/include/sys/_null.h /usr/include/strings.h \
+  rtbuf.h rtbuf_defs.h rtbuf_type.h rtbuf_fun.h symbol.h rtbuf_lib.h \
+  rtbuf_signal.h rtbuf_music.h rtbuf_signal_type.h rtbuf_sndio.h \
+  rtbuf_sndio_type.h
+rtbuf_sndio_input.o: rtbuf_sndio_input.c /usr/include/sndio.h \
+  /usr/include/sys/types.h /usr/include/sys/cdefs.h \
+  /usr/include/machine/cdefs.h /usr/include/sys/endian.h \
+  /usr/include/sys/_endian.h /usr/include/sys/_types.h \
+  /usr/include/machine/_types.h /usr/include/machine/endian.h \
+  /usr/include/stdio.h /usr/include/sys/_null.h /usr/include/strings.h \
+  rtbuf.h rtbuf_defs.h rtbuf_type.h rtbuf_fun.h symbol.h rtbuf_signal.h \
+  rtbuf_music.h rtbuf_sndio.h
+rtbuf_sndio_output.o: rtbuf_sndio_output.c /usr/include/limits.h \
+  /usr/include/sys/cdefs.h /usr/include/machine/cdefs.h \
+  /usr/include/sys/limits.h /usr/include/machine/limits.h \
+  /usr/include/sys/syslimits.h /usr/include/sndio.h \
+  /usr/include/sys/types.h /usr/include/sys/endian.h \
+  /usr/include/sys/_endian.h /usr/include/sys/_types.h \
+  /usr/include/machine/_types.h /usr/include/machine/endian.h \
+  /usr/include/stdio.h /usr/include/sys/_null.h /usr/include/strings.h \
+  rtbuf.h rtbuf_defs.h rtbuf_type.h rtbuf_fun.h symbol.h rtbuf_sndio.h \
+  rtbuf_signal.h rtbuf_music.h
+rtbuf_glfw3.o: rtbuf_glfw3.c /usr/include/stdio.h \
+  /usr/include/sys/cdefs.h /usr/include/machine/cdefs.h \
+  /usr/include/sys/_null.h /usr/include/sys/_types.h \
+  /usr/include/machine/_types.h /usr/include/sys/types.h \
+  /usr/include/sys/endian.h /usr/include/sys/_endian.h \
+  /usr/include/machine/endian.h /usr/include/string.h \
+  /usr/include/strings.h rtbuf.h rtbuf_defs.h rtbuf_type.h rtbuf_fun.h \
+  symbol.h rtbuf_lib.h rtbuf_signal.h rtbuf_music.h rtbuf_glfw3.h \
+  /usr/local/include/GLFW/glfw3.h /usr/include/stddef.h \
+  /usr/include/stdint.h /usr/X11R6/include/GL/gl.h \
+  /usr/X11R6/include/GL/glext.h /usr/include/inttypes.h \
+  /usr/include/sys/stdint.h
+rtbuf_music.o: rtbuf_music.c rtbuf.h rtbuf_defs.h rtbuf_type.h \
+  rtbuf_fun.h symbol.h rtbuf_music.h
