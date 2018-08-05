3e77bcf5553c71a90590f00393f7ccc95ea284a0
Thomas de Grivel <thoxdg@gmail.com>
2018-08-05 19:43:44 +0200

    fix build

--- dependencies.mk
+++ dependencies.mk
@@ -1,17 +1,17 @@
-symbol.o: symbol.c /usr/include/stdio.h /usr/include/sys/cdefs.h \
+symbol.$(OBJEXT): symbol.c /usr/include/stdio.h /usr/include/sys/cdefs.h \
   /usr/include/machine/cdefs.h /usr/include/sys/_null.h \
   /usr/include/sys/_types.h /usr/include/machine/_types.h \
   /usr/include/sys/types.h /usr/include/sys/endian.h \
   /usr/include/sys/_endian.h /usr/include/machine/endian.h \
   /usr/include/string.h /usr/include/strings.h symbol.h
-rtbuf_type.o: rtbuf_type.c /usr/include/stdio.h /usr/include/sys/cdefs.h \
+rtbuf_type.$(OBJEXT): rtbuf_type.c /usr/include/stdio.h /usr/include/sys/cdefs.h \
   /usr/include/machine/cdefs.h /usr/include/sys/_null.h \
   /usr/include/sys/_types.h /usr/include/machine/_types.h \
   /usr/include/sys/types.h /usr/include/sys/endian.h \
   /usr/include/sys/_endian.h /usr/include/machine/endian.h \
   /usr/include/stdlib.h /usr/include/string.h /usr/include/strings.h \
   rtbuf_type.h rtbuf_defs.h symbol.h
-rtbuf_fun.o: rtbuf_fun.c /usr/include/assert.h /usr/include/sys/cdefs.h \
+rtbuf_fun.$(OBJEXT): rtbuf_fun.c /usr/include/assert.h /usr/include/sys/cdefs.h \
   /usr/include/machine/cdefs.h /usr/include/stdio.h \
   /usr/include/sys/_null.h /usr/include/sys/_types.h \
   /usr/include/machine/_types.h /usr/include/sys/types.h \
@@ -19,7 +19,7 @@ rtbuf_fun.o: rtbuf_fun.c /usr/include/assert.h /usr/include/sys/cdefs.h \
   /usr/include/machine/endian.h /usr/include/stdlib.h \
   /usr/include/strings.h rtbuf.h rtbuf_defs.h rtbuf_type.h rtbuf_fun.h \
   symbol.h
-rtbuf.o: rtbuf.c /usr/include/assert.h /usr/include/sys/cdefs.h \
+rtbuf.$(OBJEXT): rtbuf.c /usr/include/assert.h /usr/include/sys/cdefs.h \
   /usr/include/machine/cdefs.h /usr/include/stdio.h \
   /usr/include/sys/_null.h /usr/include/sys/_types.h \
   /usr/include/machine/_types.h /usr/include/sys/types.h \
@@ -27,7 +27,7 @@ rtbuf.o: rtbuf.c /usr/include/assert.h /usr/include/sys/cdefs.h \
   /usr/include/machine/endian.h /usr/include/stdlib.h \
   /usr/include/string.h /usr/include/strings.h rtbuf.h rtbuf_defs.h \
   rtbuf_type.h rtbuf_fun.h symbol.h
-rtbuf_lib.o: rtbuf_lib.c /usr/include/assert.h /usr/include/sys/cdefs.h \
+rtbuf_lib.$(OBJEXT): rtbuf_lib.c /usr/include/assert.h /usr/include/sys/cdefs.h \
   /usr/include/machine/cdefs.h /usr/include/dlfcn.h /usr/include/stdio.h \
   /usr/include/sys/_null.h /usr/include/sys/_types.h \
   /usr/include/machine/_types.h /usr/include/sys/types.h \
@@ -35,7 +35,7 @@ rtbuf_lib.o: rtbuf_lib.c /usr/include/assert.h /usr/include/sys/cdefs.h \
   /usr/include/machine/endian.h /usr/include/stdlib.h \
   /usr/include/string.h /usr/include/strings.h rtbuf.h rtbuf_defs.h \
   rtbuf_type.h rtbuf_fun.h symbol.h rtbuf_lib.h
-rtbuf_cli.o: rtbuf_cli.c /usr/include/assert.h /usr/include/sys/cdefs.h \
+rtbuf_cli.$(OBJEXT): rtbuf_cli.c /usr/include/assert.h /usr/include/sys/cdefs.h \
   /usr/include/machine/cdefs.h /usr/local/include/cli.h \
   /usr/include/pthread.h /usr/include/sys/types.h \
   /usr/include/sys/endian.h /usr/include/sys/_endian.h \
@@ -49,9 +49,9 @@ rtbuf_cli.o: rtbuf_cli.c /usr/include/assert.h /usr/include/sys/cdefs.h \
   /usr/include/sched.h /usr/include/stdlib.h /usr/include/stdio.h \
   /usr/include/unistd.h /usr/include/sys/unistd.h rtbuf.h rtbuf_defs.h \
   rtbuf_type.h rtbuf_fun.h symbol.h rtbuf_lib.h
-rtbuf_music.o: rtbuf_music.c rtbuf.h rtbuf_defs.h rtbuf_type.h \
+rtbuf_music.$(OBJEXT): rtbuf_music.c rtbuf.h rtbuf_defs.h rtbuf_type.h \
   rtbuf_fun.h symbol.h rtbuf_music.h
-rtbuf_signal.o: rtbuf_signal.c /usr/include/math.h \
+rtbuf_signal.$(OBJEXT): rtbuf_signal.c /usr/include/math.h \
   /usr/include/sys/_types.h /usr/include/machine/_types.h \
   /usr/include/sys/limits.h /usr/include/sys/cdefs.h \
   /usr/include/machine/cdefs.h /usr/include/machine/limits.h \
@@ -60,7 +60,7 @@ rtbuf_signal.o: rtbuf_signal.c /usr/include/math.h \
   /usr/include/machine/endian.h rtbuf.h rtbuf_defs.h rtbuf_type.h \
   rtbuf_fun.h symbol.h rtbuf_lib.h rtbuf_music.h rtbuf_signal.h \
   rtbuf_signal_type.h
-rtbuf_signal_sinus.o: rtbuf_signal_sinus.c /usr/include/math.h \
+rtbuf_signal_sinus.$(OBJEXT): rtbuf_signal_sinus.c /usr/include/math.h \
   /usr/include/sys/_types.h /usr/include/machine/_types.h \
   /usr/include/sys/limits.h /usr/include/sys/cdefs.h \
   /usr/include/machine/cdefs.h /usr/include/machine/limits.h \
@@ -68,7 +68,7 @@ rtbuf_signal_sinus.o: rtbuf_signal_sinus.c /usr/include/math.h \
   /usr/include/sys/endian.h /usr/include/sys/_endian.h \
   /usr/include/machine/endian.h rtbuf.h rtbuf_defs.h rtbuf_type.h \
   rtbuf_fun.h symbol.h rtbuf_signal.h rtbuf_music.h
-rtbuf_signal_square.o: rtbuf_signal_square.c /usr/include/math.h \
+rtbuf_signal_square.$(OBJEXT): rtbuf_signal_square.c /usr/include/math.h \
   /usr/include/sys/_types.h /usr/include/machine/_types.h \
   /usr/include/sys/limits.h /usr/include/sys/cdefs.h \
   /usr/include/machine/cdefs.h /usr/include/machine/limits.h \
@@ -76,9 +76,9 @@ rtbuf_signal_square.o: rtbuf_signal_square.c /usr/include/math.h \
   /usr/include/sys/endian.h /usr/include/sys/_endian.h \
   /usr/include/machine/endian.h rtbuf.h rtbuf_defs.h rtbuf_type.h \
   rtbuf_fun.h symbol.h rtbuf_signal.h rtbuf_music.h
-rtbuf_signal_adsr.o: rtbuf_signal_adsr.c rtbuf.h rtbuf_defs.h \
+rtbuf_signal_adsr.$(OBJEXT): rtbuf_signal_adsr.c rtbuf.h rtbuf_defs.h \
   rtbuf_type.h rtbuf_fun.h symbol.h rtbuf_signal.h rtbuf_music.h
-rtbuf_signal_synth.o: rtbuf_signal_synth.c /usr/include/stdio.h \
+rtbuf_signal_synth.$(OBJEXT): rtbuf_signal_synth.c /usr/include/stdio.h \
   /usr/include/sys/cdefs.h /usr/include/machine/cdefs.h \
   /usr/include/sys/_null.h /usr/include/sys/_types.h \
   /usr/include/machine/_types.h /usr/include/sys/types.h \
@@ -86,7 +86,8 @@ rtbuf_signal_synth.o: rtbuf_signal_synth.c /usr/include/stdio.h \
   /usr/include/machine/endian.h /usr/include/strings.h rtbuf.h \
   rtbuf_defs.h rtbuf_type.h rtbuf_fun.h symbol.h rtbuf_signal.h \
   rtbuf_music.h
-rtbuf_sndio.o: rtbuf_sndio.c /usr/include/sndio.h \
+rtbuf_signal_type.$(OBJEXT): rtbuf_signal_type.h
+rtbuf_sndio.$(OBJEXT): rtbuf_sndio.c /usr/include/sndio.h \
   /usr/include/sys/types.h /usr/include/sys/cdefs.h \
   /usr/include/machine/cdefs.h /usr/include/sys/endian.h \
   /usr/include/sys/_endian.h /usr/include/sys/_types.h \
@@ -95,7 +96,7 @@ rtbuf_sndio.o: rtbuf_sndio.c /usr/include/sndio.h \
   rtbuf.h rtbuf_defs.h rtbuf_type.h rtbuf_fun.h symbol.h rtbuf_lib.h \
   rtbuf_signal.h rtbuf_music.h rtbuf_signal_type.h rtbuf_sndio.h \
   rtbuf_sndio_type.h
-rtbuf_sndio_input.o: rtbuf_sndio_input.c /usr/include/sndio.h \
+rtbuf_sndio_input.$(OBJEXT): rtbuf_sndio_input.c /usr/include/sndio.h \
   /usr/include/sys/types.h /usr/include/sys/cdefs.h \
   /usr/include/machine/cdefs.h /usr/include/sys/endian.h \
   /usr/include/sys/_endian.h /usr/include/sys/_types.h \
@@ -103,7 +104,7 @@ rtbuf_sndio_input.o: rtbuf_sndio_input.c /usr/include/sndio.h \
   /usr/include/stdio.h /usr/include/sys/_null.h /usr/include/strings.h \
   rtbuf.h rtbuf_defs.h rtbuf_type.h rtbuf_fun.h symbol.h rtbuf_signal.h \
   rtbuf_music.h rtbuf_sndio.h
-rtbuf_sndio_output.o: rtbuf_sndio_output.c /usr/include/limits.h \
+rtbuf_sndio_output.$(OBJEXT): rtbuf_sndio_output.c /usr/include/limits.h \
   /usr/include/sys/cdefs.h /usr/include/machine/cdefs.h \
   /usr/include/sys/limits.h /usr/include/machine/limits.h \
   /usr/include/sys/syslimits.h /usr/include/sndio.h \
@@ -113,7 +114,8 @@ rtbuf_sndio_output.o: rtbuf_sndio_output.c /usr/include/limits.h \
   /usr/include/stdio.h /usr/include/sys/_null.h /usr/include/strings.h \
   rtbuf.h rtbuf_defs.h rtbuf_type.h rtbuf_fun.h symbol.h rtbuf_sndio.h \
   rtbuf_signal.h rtbuf_music.h
-rtbuf_glfw3.o: rtbuf_glfw3.c /usr/include/stdio.h \
+rtbuf_sndio_type.$(OBJEXT): rtbuf_sndio_type.h
+rtbuf_glfw3.$(OBJEXT): rtbuf_glfw3.c /usr/include/stdio.h \
   /usr/include/sys/cdefs.h /usr/include/machine/cdefs.h \
   /usr/include/sys/_null.h /usr/include/sys/_types.h \
   /usr/include/machine/_types.h /usr/include/sys/types.h \
@@ -125,5 +127,5 @@ rtbuf_glfw3.o: rtbuf_glfw3.c /usr/include/stdio.h \
   /usr/include/stdint.h /usr/X11R6/include/GL/gl.h \
   /usr/X11R6/include/GL/glext.h /usr/include/inttypes.h \
   /usr/include/sys/stdint.h
-rtbuf_music.o: rtbuf_music.c rtbuf.h rtbuf_defs.h rtbuf_type.h \
+rtbuf_music.$(OBJEXT): rtbuf_music.c rtbuf.h rtbuf_defs.h rtbuf_type.h \
   rtbuf_fun.h symbol.h rtbuf_music.h
