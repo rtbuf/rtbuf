9ee03b7ff6c75946c8fe558197bed405668077a2
Thomas de Grivel <thoxdg@gmail.com>
2020-01-21 18:29:41 +0100

    wip popup menu

--- rtbuf_gtk.c
+++ rtbuf_gtk.c
@@ -2,12 +2,18 @@
 #include <stdio.h>
 
 void
+rtbuf_gtk_modular_popup_rtbuf ()
+{
+  printf("rtbuf\n");
+}
+
+void
 rtbuf_gtk_modular_bg_draw (GtkWidget *widget,
 			   cairo_t *cr)
 {
-	(void) widget;
-	(void) cr;
-	printf("draw modular bg\n");
+  (void) widget;
+  (void) cr;
+  printf("draw modular bg\n");
 }
 
 int
@@ -19,23 +25,22 @@ main (int   argc,
   GObject *button;
   GError *error = NULL;
 
-  gtk_init (&argc, &argv);
+  gtk_init(&argc, &argv);
 
   /* Construct a GtkBuilder instance and load our UI description */
   builder = gtk_builder_new ();
-  if (gtk_builder_add_from_file (builder, "rtbuf.glade", &error) == 0)
-    {
-      g_printerr ("Error loading file: %s\n", error->message);
-      g_clear_error (&error);
-      return 1;
-    }
+  if (gtk_builder_add_from_file(builder, "rtbuf.glade", &error) == 0) {
+    g_printerr("Error loading file: %s\n", error->message);
+    g_clear_error(&error);
+    return 1;
+  }
 
   /* Connect signal handlers to the constructed widgets. */
-  window = gtk_builder_get_object (builder, "modular");
-  g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+  window = gtk_builder_get_object(builder, "modular");
+  g_signal_connect(window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
-  button = gtk_builder_get_object (builder, "quit");
-  g_signal_connect (button, "activate", G_CALLBACK (gtk_main_quit), NULL);
+  button = gtk_builder_get_object(builder, "quit");
+  g_signal_connect(button, "activate", G_CALLBACK (gtk_main_quit), NULL);
 
   gtk_main ();
 
