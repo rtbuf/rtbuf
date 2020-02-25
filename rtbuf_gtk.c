dbd1aed45070f06804748dae6356168f243ca88c
Thomas de Grivel <thoxdg@gmail.com>
2020-02-25 19:10:14 +0100

    wip rtbuf-gtk

--- rtbuf_gtk.c
+++ rtbuf_gtk.c
@@ -1,28 +1,56 @@
 #include <gtk/gtk.h>
 #include <stdio.h>
 
-void
-rtbuf_gtk_modular_popup_rtbuf ()
+GObject *modular;
+GObject *modular_layout;
+
+void rtbuf_gtk_modular_close (GtkWidget *widget,
+                              gpointer user_data)
 {
-  printf("rtbuf\n");
+  printf("rtbuf-gtk modular close\n");
+  gtk_main_quit();
 }
 
-void
-rtbuf_gtk_modular_bg_draw (GtkWidget *widget,
-			   cairo_t *cr)
+gboolean rtbuf_gtk_modular_popup (GtkWidget *widget,
+                                  GdkEventButton *event,
+                                  gpointer user_data)
+{
+  if (event->button == 3) {
+    printf("rtbuf-gtk modular popup\n");
+  }
+  return TRUE;
+}
+
+gboolean rtbuf_gtk_modular_draw (GtkWidget *widget,
+                                 cairo_t *cr,
+                                 gpointer user_data)
 {
   (void) widget;
   (void) cr;
-  printf("draw modular bg\n");
+  (void) user_data;
+  printf("rtbuf-gtk modular draw\n");
+  return TRUE;
 }
 
-int
-main (int   argc,
-      char *argv[])
+void rtbuf_gtk_modular_window (GtkBuilder *builder)
+{
+  GObject *button;
+
+  modular = gtk_builder_get_object(builder, "modular");
+  g_signal_connect(modular, "destroy", G_CALLBACK(rtbuf_gtk_modular_close), NULL);
+
+  button = gtk_builder_get_object(builder, "quit");
+  g_signal_connect(button, "activate", G_CALLBACK(rtbuf_gtk_modular_close), NULL);
+
+  modular_layout = gtk_builder_get_object(builder, "modular_layout");
+  gtk_widget_add_events(modular_layout, GDK_BUTTON_PRESS_MASK);
+  g_signal_connect(modular_layout, "button-press-event", G_CALLBACK(rtbuf_gtk_modular_popup), NULL);
+  g_signal_connect(modular_layout, "draw", G_CALLBACK(rtbuf_gtk_modular_draw), NULL);
+}
+
+int main (int argc, char *argv[])
 {
   GtkBuilder *builder;
-  GObject *window;
-  GObject *button;
   GError *error = NULL;
 
   gtk_init(&argc, &argv);
@@ -35,12 +63,7 @@ main (int   argc,
     return 1;
   }
 
-  /* Connect signal handlers to the constructed widgets. */
-  window = gtk_builder_get_object(builder, "modular");
-  g_signal_connect(window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
-
-  button = gtk_builder_get_object(builder, "quit");
-  g_signal_connect(button, "activate", G_CALLBACK (gtk_main_quit), NULL);
+  rtbuf_gtk_modular_window(builder);
 
   gtk_main ();
 
