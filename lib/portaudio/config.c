cf3e8034798fe20748a32ac0fc27e11de5153a10
Thomas de Grivel <thoxdg@gmail.com>
2022-11-03 10:32:39 +0100

    licence

--- lib/portaudio/config.c
+++ lib/portaudio/config.c
@@ -1,3 +1,16 @@
+/* c3
+ * Copyright 2018-2022 kmx.io <contact@kmx.io>
+ *
+ * Permission is hereby granted to use this software granted
+ * the above copyright notice and this permission paragraph
+ * are included in all copies and substantial portions of this
+ * software.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS-IS" WITHOUT ANY GUARANTEE OF
+ * PURPOSE AND PERFORMANCE. IN NO EVENT WHATSOEVER SHALL THE
+ * AUTHOR BE CONSIDERED LIABLE FOR THE USE AND PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
 #include <portaudio.h>
 
 int main ()
